14e96d09e5041d3e404f55425102af9a7095a8c5
Manoj Maddipoti <manojm.apps@gmail.com>
2025-12-25 09:28:40 -0500

    Changed title to include Crypto

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if "db_init" not in st.session_state:
 
 # --- 2. Sidebar ---
 with st.sidebar:
-    st.title("💰 Market AI Intelligence")
+    st.title("💰 Market AI PRO Intelligence")
     if st.button("🗑️ Reset All Memory"):
         db.clear_db()
         st.rerun()
